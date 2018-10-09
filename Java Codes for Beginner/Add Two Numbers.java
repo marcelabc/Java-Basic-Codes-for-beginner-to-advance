a28bd46c3627f6586e4a7084ca8163dcc1796cdc
marcela cunha <mbc3@cin.ufpe.br>
2018-10-09 17:19:53 -0300

    recomenço

--- Java Codes for Beginner/Add Two Numbers.java
+++ Java Codes for Beginner/Add Two Numbers.java
@@ -8,11 +8,5 @@ public class add{
         int sum = first + second;
 
         System.out.println("The sum is: " + sum);
-        
-        int resultado = 50;
-        int mult = resultado * 2;
-        
-        int divisor = 2;
-        int divisao = sum / divisor;
     }
 }
